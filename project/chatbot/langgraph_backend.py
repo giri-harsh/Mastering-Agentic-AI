d9b5176638a71b96b75ee83ca3b0289403dab663
Harsh Giri <2006.harshgiri@gmail.com>
2026-07-23 21:59:32 +0530

    New file

---
+++ project/chatbot/langgraph_backend.py
@@ -0,0 +1,35 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_groq import ChatGroq
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import sqlite3
+
+load_dotenv()
+
+conn = sqlite3.connect(database='chatbot.db',check_same_thread=False)
+llm = ChatGroq(
+    model="meta-llama/llama-4-scout-17b-16e-instruct"
+)
+
+
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+# Checkpointer
+checkpointer = SqliteSaver(conn= conn)
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
